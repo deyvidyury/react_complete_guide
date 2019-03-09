22f38afc60f594b0c253c48fa0ea93dce481b960
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-03-09 11:59:54 -0300

    configuracoes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,33 +1,28 @@
-import React, {
-  Component
-} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephane", age: 26 }
+    ]
+  };
+
+  switchNameHandler = () => {};
   render() {
-    return ( <
-      div className = "App" >
-      <
-      header className = "App-header" >
-      <
-      img src = {
-        logo
-      }
-      className = "App-logo"
-      alt = "logo" / >
-      <
-      p >
-      Edit < code > src / App.js < /code> and save to reload. <
-      /p> <
-      a className = "App-link"
-      href = "https://reactjs.org"
-      target = "_blank"
-      rel = "noopener noreferrer" >
-      Learn React <
-      /a> <
-      /header> <
-      /div>
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name="Max" age="28" />
+        <Person name="MAnu" age="29">
+          My hobbies: Racing
+        </Person>
+        <Person name="Stephanie" age="26" />
+      </div>
     );
   }
 }
