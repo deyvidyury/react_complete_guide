4b5531e2e7b4cf864275c87373fc6760b7d12dfd
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-03-11 17:55:46 -0300

    Styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,17 +11,56 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {};
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Maximilliam", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephane", age: 27 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephane", age: 26 }
+      ]
+    });
+  };
+
   render() {
+    const style = {
+      backgroudColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name="Max" age="28" />
-        <Person name="MAnu" age="29">
+        <button onClick={this.switchNameHandler} style={style}>
+          Switch name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
+        >
           My hobbies: Racing
         </Person>
-        <Person name="Stephanie" age="26" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
